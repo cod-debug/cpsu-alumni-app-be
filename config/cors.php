63c3281f5a59ce0e1dd556427349fac3079bad68
Roy Duenas <rduenas@ingenuiti.com>
2024-07-09 18:38:32 +0800

    restore cors policy

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://*.cpsualumniapp.online', 'https://cpsualumniapp.online'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
