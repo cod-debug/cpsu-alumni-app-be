52f887c0fa0f41b2e7ce8844a1d22729eaa03002
Roy Duenas <rduenas@ingenuiti.com>
2024-07-09 18:36:01 +0800

    Updated cors policy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://*.cpsualumniapp.online'],
 
     'allowed_origins_patterns' => [],
 
